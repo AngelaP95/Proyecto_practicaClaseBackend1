ac976d14e23f6166a5833f965d33025ddc0060f1
AngelaP95 <apachecobe@cesde.net>
2026-02-07 11:35:24 -0500

    feat: US-001 cretae crub product

---
+++ src/main/java/CostumerSystem/SellingSystem.java
@@ -0,0 +1,89 @@
+package CostumerSystem;
+
+import java.util.Scanner;
+
+public class SellingSystem {
+
+   static Scanner sc = new Scanner(System.in);
+
+    //Product Atributes
+
+   static int id;
+   static String description;
+   static double price;
+   static int quantity;
+   static boolean state;
+
+
+   public static void main(String[] args) {
+       createProducts();
+       getProducts();
+       System.out.println("Ingrese el Id del producto a actualizar");
+       int id = sc.nextInt();
+       sc.nextLine();
+       updateProducts(id);
+       deleteProducts(id);
+
+
+    }
+
+    // Clas Methods
+
+    public static void createProducts (){
+
+        System.out.println("Ingrese el id del producto");
+        id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el nombre del producto");
+        description = sc.nextLine();
+        System.out.println("Ingrese el precio del producto");
+        price = sc.nextDouble();
+        System.out.println("Ingrese la cantidad");
+        quantity = sc.nextInt();
+        System.out.println("Ingrese el estado del producto");
+        state = sc.nextBoolean();
+    }
+
+    public static void  getProducts (){
+        System.out.println("id: "+id + "\n" +
+                "description: " + description + "\n" +
+                "price: " + price + "\n" +
+                "quantity: " + quantity + "\n" +
+                "state: " + state + "\n" );
+    }
+
+    public static void updateProducts(int findId){
+
+       if(findId== id){
+
+        System.out.println("Ingrese el id del producto");
+        id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el nombre del producto");
+        description = sc.nextLine();
+        System.out.println("Ingrese el precio del producto");
+        price = sc.nextDouble();
+        System.out.println("Ingrese la cantidad");
+        quantity = sc.nextInt();
+        System.out.println("Ingrese el estado del producto");
+        state = sc.nextBoolean();
+    }
+       else {
+           System.out.println("Producto no encontrado");
+       }
+}
+    public static void deleteProducts (int deleteId){
+       if (deleteId == id) {
+           id = 0;
+           description = " ";
+           price = 0.0;
+           quantity = 0;
+           state = false;
+       }
+       else {
+           System.out.println("Id no encontrado");
+       }
+
+    }
+
+}
